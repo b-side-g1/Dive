bc6b4396f092f10e0a1ee2d47100a3a221b1170d
Terry.Joo <jooyungik@gmail.com>
2020-06-07 15:43:25 +0900

    Add todo comments

--- lib/pages/InputListPage.dart
+++ lib/pages/InputListPage.dart
@@ -10,6 +10,12 @@ class InputListPage extends StatefulWidget {
 
 class _InputListPageState extends State<InputListPage> {
   List<Row> rows = [
+<<<<<<< HEAD
+=======
+    /* TODO: 만약 [기획](https://www.notion.so/1-59ce2cbc792445d08d2fe96c3e9b0d6e)의 입력 리스트가 확정되면 이 곳에 일일 데이터 연동
+    아래는 개발용 데이터*/
+
+>>>>>>> d8b11bd... Add todo comments
 //    Row(children: <Widget>[Text('YYYY-MM-DD'), Text('점수: 1')]),
 //    Row(children: <Widget>[Text('YYYY-MM-DD'), Text('점수: 2')]),
 //    Row(children: <Widget>[Text('YYYY-MM-DD'), Text('점수: 3')]),
