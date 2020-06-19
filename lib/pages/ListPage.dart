6f080545d2cf992508f67fd58a359595b62bb00f
yeokyeong <yeokyeong1004@gmail.com>
2020-06-19 22:33:01 +0900

    [yk]add list

--- lib/pages/ListPage.dart
+++ lib/pages/ListPage.dart
@@ -12,17 +12,82 @@ class _ListPageState extends State<ListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment
-              .spaceAround, // 주 방향(여기서는 Row니 가로입니다)으로 어떻게 위젯을 배열할지 정합니다. MainAxisAlignment.spaceAround는 위젯의 간격이 서로 일정하게 벌려주는 걸 말합니다.
-          children: <Widget>[
-            Text("나의 첫 플러터 앱"),
-            Text("이제 앱 개발 해보자"),
-            Icon(Icons.videocam, color: Colors.amber),
-          ],
-        ));
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: BodyLayout(),
+    );
+  }
+
+  final TextStyle menuFont = const TextStyle(fontSize: 18.0);
+  final button1 =
+      new RaisedButton(child: new Text("Button1"), onPressed: () {});
+
+  // Widget _buildMenu() {
+  //   return ListView.builder(
+  //       padding: const EdgeInsets.all(16.0),
+  //       itemBuilder: (BuildContext context, int index) {
+  //         // if (index.isOdd) return Divider();
+  //         // final index = index ~/ 2;
+
+  //     // return Container(width: 200, height: 200, margin: EdgeInsets.only(bottom: 10), color: Colors.red);
+  //       });
+  // }
+
+  // Widget _menuItem() {
+  //   print(pair);
+  //   return ListTile(
+  //     title: Text(
+  //       pair.asPascalCase,
+  //       style: menuFont,
+  //     ),
+  //     trailing: Icon(isAlreadySaved ? Icons.favorite : Icons.favorite_border,
+  //         color: isAlreadySaved ? Colors.red : null),
+  //   );
+  // }
+
+}
+
+class BodyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView(context);
   }
 }
+
+Widget _myListView(BuildContext context) {
+  // final menus = ['오늘 나의 점수', '그래프'];
+// return ListView.separated( itemCount: menus.length, itemBuilder: (context, index) { return ListTile( title: Text(menus[index]), ); }, separatorBuilder: (context, index) { return Divider(); }, );
+
+  return ListView(
+    children: ListTile.divideTiles(
+      context: context,
+      tiles: [
+        ListTile(
+          title: Text('오늘 나의 점수'),
+        ),
+        ListTile(
+          title: Text('그래프'),
+        ),
+        ListTile(
+          title: Text('캘린더'),
+        ),
+        ListTile(
+          title: Text('통계페이지로 이동'),
+        ),
+        ListTile(
+          title: Text('무드등 켜기'),
+        ),
+      ],
+    ).toList(),
+  );
+}
+
+// section - today score
+// class ListPage extends StatefulWidget {
+//   ListPage({Key key, this.title}) : super(key: key);
+//   final String title;
+
+//   @override
+//   _ListPageState createState() => _ListPageState();
+// }
