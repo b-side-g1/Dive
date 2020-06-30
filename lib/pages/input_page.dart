6b1d185bea37512393327e24d34031c14ea46f34
Terry.Joo <jooyungik@gmail.com>
2020-07-01 00:31:36 +0900

    Add Input page background(step1,2,3)

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterapp/components/forms/recording_form.dart';
 
 class InputPage extends StatefulWidget {
   InputPage() : super();
@@ -12,9 +11,21 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: RecordingForm(),
-      );
-  }
+    int step = 1;
+    _setBackGround() {
+      switch (step) {
+        case 1:
+          return Color.fromRGBO(43, 99, 194, 1.0);
+        case 2:
+          return Color.fromRGBO(20, 69, 151, 1.0);
+        case 3:
+          return Color.fromRGBO(12, 48, 107, 1.0);
+      }
+    }
 
+    return Container(
+      decoration: BoxDecoration(color: _setBackGround()),
+      child: null,
+    );
+  }
 }
