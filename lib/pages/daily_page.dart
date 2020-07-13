51e8001dce5caecafd5ef3cde8a1e4420026d88a
강석윤 <james.kang@gooroomee.com>
2020-07-14 02:26:32 +0900

    setState 적용

--- lib/pages/daily_page.dart
+++ lib/pages/daily_page.dart
@@ -3,15 +3,14 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/components/record_card.dart';
+import 'package:flutterapp/models/basic_model.dart';
 import 'package:flutterapp/models/daily_model.dart';
 import 'package:flutterapp/models/record_model.dart';
 import 'package:flutterapp/pages/setting_page.dart';
+import 'package:flutterapp/services/basic/basic_service.dart';
 import 'package:flutterapp/services/daily/daily_service.dart';
-import 'package:flutterapp/services/emotion/emotion_service.dart';
 import 'package:flutterapp/services/record/record_service.dart';
-import 'package:flutterapp/services/tag/tag_service.dart';
 import 'package:intl/intl.dart';
-import 'package:random_string/random_string.dart';
 
 import 'input_page.dart';
 
@@ -20,16 +19,17 @@ class DailyPage extends StatefulWidget {
 }
 
 class _DailyPageState extends State<DailyPage> {
+  Basic _basic;
   DateTime _today = DateTime.now();
   DateTime _date = DateTime.now();
   int _dailyScore = 0;
-  Daily _daily;
   List<Record> _recordList;
+  bool isToday = true;
+  bool isEmpty = true;
 
+  BasicService _basicService = BasicService();
   RecordService _recordService = RecordService();
   DailyService _dailyService = DailyService();
-  EmotionService _emotionService = EmotionService();
-  TagService _tagService = TagService();
 
   @override
   void dispose() {}
@@ -37,58 +37,31 @@ class _DailyPageState extends State<DailyPage> {
   @override
   void initState() {
     super.initState();
-    _setData();
+    _setDataByDate(_date);
   }
 
-  _setData() {
-    _setDailyByDate(_date);
-//    _setRecordList();
-//    _setDailyScore();
-  }
+  void _setDataByDate(DateTime date) async {
+    var resDaily = await _dailyService.selectDailyByDate(date);
 
-  void _setDailyByDate(DateTime date) async {
-    // TODO: 원래는 Basic model에서 하루 마감시간으로 갖고 와야 함, 현재는 startAt: 00:00:00, endAt: 23:59:59
-    _daily = await _dailyService.selectDailyByDate(date);
-    if (_daily == null) {
-      _daily = Daily(
-        id: randomString(20),
-        startAt: DateTime(date.year, date.month, date.day, 0, 0, 0).toIso8601String(),
-        endAt: DateTime(date.year, date.month, date.day, 23, 59, 59).toIso8601String(),
-        weekday: date.weekday,
-        day: date.day,
-        week: weekNumber(date),
-        month: date.month,
-        year: date.year,
-      );
-      await _dailyService.insertDaily(_daily);
-    }
-    _recordList = await _recordService.selectAllByDailyId(_daily.id);
-    _recordList.forEach((_record) async {
-      _record.emotions = await _emotionService.selectEmotionAllByRecordId(_record.id);
-      _record.tags = await _tagService.selectTagAllByRecordId(_record.id);
+    var resRecords = resDaily == null
+        ? List<Record>()
+        : await _recordService
+            .selectAllWithEmotionsAndTagsByDailyId(resDaily.id);
+
+    var resDailyScore = resRecords.isEmpty
+        ? 0
+        : (resRecords.map((c) => c.score).reduce((a, b) => a + b) /
+                resRecords.length)
+            .round();
+
+    var resIsToday = _today.difference(date).inDays == 0;
+    var resIsEmpty = resRecords.length == 0;
+    setState(() {
+      _recordList = resRecords;
+      _dailyScore = resDailyScore;
+      isToday = resIsToday;
+      isEmpty = resIsEmpty;
     });
-
-    _dailyScore = _recordList == null || _recordList.isEmpty
-        ? 0 :
-        (_recordList.map((c) => c.score).reduce((a, b) => a + b) /
-        _recordList.length).round();
-  }
-
-  void _setRecordList() async {
-    if (_daily == null) return;
-
-    _recordList = await _recordService.selectAllByDailyId(_daily.id);
-    _recordList.forEach((_record) async {
-      _record.emotions = await _emotionService.selectEmotionAllByRecordId(_record.id);
-      _record.tags = await _tagService.selectTagAllByRecordId(_record.id);
-    });
-  }
-
-  void _setDailyScore() {
-    _dailyScore = _recordList != null
-        ? (_recordList.map((c) => c.score).reduce((a, b) => a + b) /
-            _recordList.length).round()
-        : 0;
   }
 
   int weekNumber(DateTime date) {
@@ -105,14 +78,11 @@ class _DailyPageState extends State<DailyPage> {
     if (picked != null)
       setState(() {
         _date = picked;
-        _setData();
+        _setDataByDate(_date);
       });
   }
 
   Widget _currentStatusContainer() {
-    bool isToday = _today.difference(_date).inDays == 0;
-    bool isEmpty = _recordList == null || _recordList.length == 0;
-
     /*
     * 1. 오늘 O, 기록 X -> Text(지금 이 순간\n나의 기분을 남겨주세요)
     * 2. 오늘 O, 기록 O -> Text(점수)\n Text(오늘 하루의 점수)
@@ -127,7 +97,8 @@ class _DailyPageState extends State<DailyPage> {
       );
     }
     if (isToday && !isEmpty) {
-      return Row(
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text(
             _dailyScore.toString(),
@@ -150,7 +121,8 @@ class _DailyPageState extends State<DailyPage> {
       );
     }
     if (!isToday && !isEmpty) {
-      return Row(
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text(
             _dailyScore.toString(),
@@ -204,7 +176,7 @@ class _DailyPageState extends State<DailyPage> {
       child: Row(
         children: <Widget>[
           Image.asset(
-            'lib/src/image/daily/Topnav_logo@3x.png', // TODO: @3x 이거 뭐죠?
+            'lib/src/image/daily/Topnav_logo.png',
             height: 16,
             fit: BoxFit.fill,
           ),
@@ -212,7 +184,7 @@ class _DailyPageState extends State<DailyPage> {
             margin: EdgeInsets.fromLTRB(273, 0, 0, 0),
             child: IconButton(
               icon: Image.asset(
-                'lib/src/image/daily/Topnav_icon_setting@3x.png',
+                'lib/src/image/daily/Topnav_icon_setting.png',
                 height: 24,
               ),
               onPressed: () {
@@ -234,18 +206,16 @@ class _DailyPageState extends State<DailyPage> {
     return FittedBox(
       fit: BoxFit.fill, // width 100% 적용!
       child: Image.asset(
-        'lib/src/image/daily/img_wave@3x.png',
+        'lib/src/image/daily/img_wave.png',
         height: 160,
       ),
     );
   }
 
   Widget _createRecordContainer(BuildContext context) {
-    bool isToday = _today.difference(_date).inDays == 0;
-    bool isEmpty = _recordList == null || _recordList.length == 0;
-    if(!isToday && isEmpty){
+    if (!isToday && isEmpty) {
       return Image.asset(
-        'lib/src/image/daily/icon_empty@3x.png',
+        'lib/src/image/daily/icon_empty.png',
         height: 140,
       );
     }
@@ -255,7 +225,7 @@ class _DailyPageState extends State<DailyPage> {
             context, MaterialPageRoute(builder: (context) => InputPage()));
       },
       child: Image.asset(
-        'lib/src/image/daily/btn_dive@3x.png',
+        'lib/src/image/daily/btn_dive.png',
         height: 140,
       ),
     );
@@ -263,9 +233,6 @@ class _DailyPageState extends State<DailyPage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isToday = _today.difference(_date).inDays == 0;
-    bool isEmpty = _recordList == null || _recordList.length == 0;
-
     return CustomScrollView(
       slivers: <Widget>[
         SliverAppBar(
@@ -290,7 +257,8 @@ class _DailyPageState extends State<DailyPage> {
           ]),
         ),
         SliverFixedExtentList(
-          itemExtent: !isToday && !isEmpty ? 0 : 150.0, // TODO: 없애야 되는데 height 0으로 변경한 거;;리팩토링 필수
+          itemExtent: !isToday && !isEmpty ? 0 : 150.0,
+          // TODO: 없애야 되는데 height 0으로 변경한 거;;리팩토링 필수
           delegate: SliverChildListDelegate([
             _createRecordContainer(context),
           ]),
@@ -303,11 +271,11 @@ class _DailyPageState extends State<DailyPage> {
                 alignment: Alignment.center,
                 padding: EdgeInsets.all(10),
                 child: RecordCard(
-                  record: this._recordList[index],
+                  record: _recordList[index],
                 ),
               );
             },
-            childCount: this._recordList == null ? 0 : this._recordList.length,
+            childCount: _recordList == null ? 0 : _recordList.length,
           ),
         ),
       ],
