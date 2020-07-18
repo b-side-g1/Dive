6daa36ab5c5aaa8bec594707709e36b8545ba252
kys6879 <kys6879@naver.com>
2020-07-18 23:24:33 +0900

    #56 impl title widget

--- lib/pages/input_page_step3.dart
+++ lib/pages/input_page_step3.dart
@@ -11,8 +11,51 @@ class _InputPageStep3State extends State<InputPageStep3> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          child: Text("Hello")
-      ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.only(top: 110, left: 70, right: 70),
+                  child: Center(
+                      child: Text("그렇게 느끼는 이유는...",style: TextStyle(
+                        fontSize: 21,
+                        fontWeight: FontWeight.w700
+                      ),))),
+              Container(
+                  padding: EdgeInsets.only(top: 30,left: 20,right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("이유태그",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w700),),
+//                      Row(
+//                        children: <Widget>[
+//                          Icon(
+//                              Icons.forward
+//                          ),
+//                          Text("태그편집"),
+//                        ],
+//                      )
+                      FlatButton(
+                        padding: EdgeInsets.all(0),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                                Icons.edit
+                            ),
+                            SizedBox(
+                              width: 3,
+                            ),
+                            Text("태그편집",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+              ),
+            ],
+          )),
     );
   }
 }
