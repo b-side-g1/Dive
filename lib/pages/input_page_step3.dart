79ee1ed8a9fb12125c198b93d4182fe5e3be2a00
kys6879 <kys6879@naver.com>
2020-07-18 14:51:01 +0900

    #56 wrapping scaffold

--- lib/pages/input_page_step3.dart
+++ lib/pages/input_page_step3.dart
@@ -9,19 +9,9 @@ class InputPageStep3 extends StatefulWidget {
 class _InputPageStep3State extends State<InputPageStep3> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage('lib/src/image/marble.png')),
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('lib/src/image/marble_shadow.png'),
-                alignment: Alignment(0.0, 0.75)),
-            border: Border.all(
-              width: 1,
-              color: Colors.yellowAccent, //
-            )),
+    return Scaffold(
+      body: Container(
+          child: Text("Hello")
       ),
     );
   }
