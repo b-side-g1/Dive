d826e776992f10e9eed1a9824d63f2487f98b251
Terry.Joo <jooyungik@gmail.com>
2020-08-01 23:49:01 +0900

    Fix format & Optimize import

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -6,7 +6,6 @@ import 'package:flutterapp/pages/input_page_step1.dart';
 import 'package:flutterapp/pages/input_page_step2.dart';
 import 'package:flutterapp/pages/input_page_step3.dart';
 import 'package:flutterapp/provider/input/tag_provider.dart';
-import 'package:flutterapp/provider/input/tag_provider.dart';
 import 'package:provider/provider.dart';
 
 class InputPage extends StatefulWidget {
@@ -16,8 +15,10 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   int currentStep = 0;
+
   // step2 디버그용 리스트. rx를 이용할 게 아니라면 아래 주석을 살려서 탑다운으로 데이터를 처리하는 게 저장이나 불러오기에 깔끔하지 않은가 싶음.
   List emotions = [];
+
 // Record를 통째로 받아서 사용할 경우 아래 주석 살려서 처리(Step2 기준)
 //  Record _record;
 //
@@ -122,7 +123,6 @@ class _InputPageState extends State<InputPage> {
 
   @override
   Widget build(BuildContext context) {
-
     final container = StateContainer.of(context);
     testScore = container.score;
 
@@ -135,23 +135,20 @@ class _InputPageState extends State<InputPage> {
               scrollDirection: Axis.vertical,
               children: [
                 InputPageStep1(handlerPageView: handlerPageView),
-              InputPageStep2(
-              // 테스트해보진 않았지만, 변경 내용을 특별한 함수나 rx방식 없이
-              // InputPage에서 step2의 변경을 트래킹할 수 있을 겁니다.
-              // 빈 리스트가 아닌 경우의 처리는 고려치 않았기에, 정상동작하지 않을 수 있습니다.
-              emotions: emotions,
-              ),
-                MultiProvider(
-                    providers: [
-                      StreamProvider<List<Tag>>.value(
-                        value: TagProvider().tags,
-                      ),
-                      Provider<TagProvider>(
-                        create: (_) => TagProvider(),
-                      )
-                    ],
-                    child: InputPageStep3()
-                )
+                InputPageStep2(
+                  // 테스트해보진 않았지만, 변경 내용을 특별한 함수나 rx방식 없이
+                  // InputPage에서 step2의 변경을 트래킹할 수 있을 겁니다.
+                  // 빈 리스트가 아닌 경우의 처리는 고려치 않았기에, 정상동작하지 않을 수 있습니다.
+                  emotions: emotions,
+                ),
+                MultiProvider(providers: [
+                  StreamProvider<List<Tag>>.value(
+                    value: TagProvider().tags,
+                  ),
+                  Provider<TagProvider>(
+                    create: (_) => TagProvider(),
+                  )
+                ], child: InputPageStep3())
               ],
               onPageChanged: (page) {
                 setState(() {
