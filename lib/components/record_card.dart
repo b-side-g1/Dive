0b3d87860a6007b0ecfb7a437d7889c9e043f2a5
강석윤 <james.kang@gooroomee.com>
2020-08-02 04:03:38 +0900

    입력 카드 리스트 변경, TODO: gridView 자체 스크롤 때문에 고정 높이값 주지 않으면 안나오는 문제 (그 외 완료)

--- lib/components/record_card.dart
+++ lib/components/record_card.dart
@@ -14,55 +14,149 @@ class RecordCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: getColorByScore(record.score),
-      child: Row(
-        children: <Widget>[
-          CustomPaint(
-              size: Size(50, 50),
-              painter: PieChart(
-                  percentage: record.score,
-                  textScaleFactor: 0.5,
-                  textColor: Colors.white,
-                  unFilledChartColor: getColorByScore(record.score),
-                  filledChartColor: Colors.white)),
-          Column(
-            children: <Widget>[
-              emotionList(record.emotions),
-              tagList(record.tags),
-              Text(record.createdAt == null ? '' : record.createdAt, //DateFormat('kk:mm').format(record.createdAt),
-                  style: TextStyle(color: Colors.white),
-                  textAlign: TextAlign.right)
-            ],
-          )
-        ],
-      ),
-    );
+        shape:
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+        color: Color(0xff2a67d0),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: IntrinsicHeight(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: CustomPaint(
+                      size: Size(56, 56),
+                      painter: PieChart(
+                          percentage: record.score,
+                          textScaleFactor: 0.8,
+                          textColor: Color(0xff33f7fe),
+                          unFilledChartColor: Color(0xff63c7ff),
+                          filledChartColor: Color(0xff33f7fe))),
+                ),
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.only(left: 20, right: 10),
+                        child: Column(
+                          children: <Widget>[
+                            Container( // TODO: ...
+                              height: this.record.emotions.isNotEmpty ? 130 : 0,
+                              child: GridView.count(
+                                crossAxisCount: 3,
+                                shrinkWrap: true,
+                                crossAxisSpacing: 25,
+                                mainAxisSpacing: 12,
+                                childAspectRatio: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .width /
+                                    (MediaQuery
+                                        .of(context)
+                                        .size
+                                        .height / 3),
+                                children: List.generate(
+                                    this.record.emotions.length, (index) {
+                                  return _textInsideCircleWidget(
+                                      this.record.emotions[index].name);
+                                }),
+                              ),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    top: this.record.emotions.isNotEmpty
+                                        ? 13
+                                        : 0),
+                                child:
+                                Container( // TODO: ...
+                                  height: this.record.tags.isNotEmpty
+                                      ? 130
+                                      : 0,
+                                  child: GridView.count(
+                                    crossAxisCount: 3,
+                                    shrinkWrap: true,
+                                    crossAxisSpacing: 25,
+                                    mainAxisSpacing: 12,
+                                    childAspectRatio: MediaQuery
+                                        .of(context)
+                                        .size
+                                        .width /
+                                        (MediaQuery
+                                            .of(context)
+                                            .size
+                                            .height / 3),
+                                    children: List.generate(
+                                        this.record.tags.length, (index) {
+                                      return _textWithHashTagWidget(
+                                          this.record.tags[index].name);
+                                    }),
+                                  ),
+                                ),
+                            ),
+                            Expanded(
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      top: this.record.emotions.isNotEmpty ||
+                                          this.record.tags.isNotEmpty
+                                          ? 13
+                                          : 0),
+                                  child: Text(
+                                    record.description,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                        fontFamily: "NotoSansKR"),
+                                  ),
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(top: 25, right: 10),
+                                child: Align(
+                                    alignment: Alignment.topRight,
+                                    child: Text(
+                                      record.createdAt == null
+                                          ? ''
+                                          : record.createdAt,
+                                      //DateFormat('kk:mm').format(record.createdAt),
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 14,
+                                          fontFamily: "NotoSans"),
+                                    ))),
+                          ],
+                        ))),
+              ],
+            ),
+          ),
+        ));
   }
 }
 
-Color getColorByScore(int score) {
-  if (score <= 20) {
-    return Colors.grey;
-  }
-  if (score <= 50) {
-    return Colors.blue;
-  }
-  if (score < 80) {
-    return Colors.blueAccent;
-  }
-  return Colors.indigoAccent;
+Widget _textInsideCircleWidget(String text) {
+  return ClipRect(
+      child: Container(
+          height: 27,
+          width: text.length * 13.0 + 30,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white, width: 1.5),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0),
+                bottomLeft: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0)),
+          ),
+          // this line makes the coffee.
+          child: Center(
+              child: Text(
+                text,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "NotoSans",
+                    fontWeight: FontWeight.w700),
+              ))));
 }
 
-Widget emotionList(List<Emotion> emotionList) {
-  if (emotionList == null) {
-    return Text("");
-  }
-  return Text(emotionList.map((e) => e.name).join('·'));
-}
-
-Widget tagList(List<Tag> tagList) {
-  if (tagList == null) {
-    return Text("");
-  }
-  return Text(tagList.map((e) => "#" + e.name).join(' '));
+_textWithHashTagWidget(String text) {
+  return Text("#" + text,
+      style: TextStyle(
+          color: Colors.white,
+          fontFamily: "NotoSans",
+          fontWeight: FontWeight.w700));
 }
