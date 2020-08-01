9796c2a4df89afc3da295048a78efedcd68af6f0
Terry.Joo <jooyungik@gmail.com>
2020-08-02 00:40:31 +0900

    Fix data format for save

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -16,15 +16,13 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   int currentStep = 0;
 
-  // step2 디버그용 리스트. rx를 이용할 게 아니라면 아래 주석을 살려서 탑다운으로 데이터를 처리하는 게 저장이나 불러오기에 깔끔하지 않은가 싶음.
-  List emotions = [];
+  List _emotions = [];
 
-// Record를 통째로 받아서 사용할 경우 아래 주석 살려서 처리(Step2 기준)
-//  Record _record;
-//
-//  get emotions => _record?.emotions;
-//
-//  set emotions(List<Emotion> values) => {_record.emotions = values};
+  set emotions(List value) {
+    _emotions = value;
+  }
+
+  get emotions => _emotions;
 
   PageController _controller = PageController(
     initialPage: 0,
@@ -136,9 +134,6 @@ class _InputPageState extends State<InputPage> {
               children: [
                 InputPageStep1(handlerPageView: handlerPageView),
                 InputPageStep2(
-                  // 테스트해보진 않았지만, 변경 내용을 특별한 함수나 rx방식 없이
-                  // InputPage에서 step2의 변경을 트래킹할 수 있을 겁니다.
-                  // 빈 리스트가 아닌 경우의 처리는 고려치 않았기에, 정상동작하지 않을 수 있습니다.
                   emotions: emotions,
                 ),
                 MultiProvider(providers: [
--- lib/pages/input_page_step2.dart
+++ lib/pages/input_page_step2.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/components/emotion_tag_box.dart';
-import 'package:flutterapp/inherited/state_container.dart';
+import 'package:flutterapp/models/emotion_model.dart';
 
 class InputPageStep2 extends StatefulWidget {
   Color background;
   List emotions;
 
-  InputPageStep2({Key key, this.background, this.emotions}) : super(key: key);
+  InputPageStep2({Key key, this.background, List emotions})
+      : emotions = emotions,
+        super(key: key);
 
   @override
   _InputPageStep2State createState() => _InputPageStep2State();
 }
 
 class _InputPageStep2State extends State<InputPageStep2> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
 //      For Debug or develop
       backgroundColor: Color.fromRGBO(19, 62, 133, 1.0),
@@ -38,7 +38,9 @@ class _InputPageStep2State extends State<InputPageStep2> {
             ),
             Padding(
               padding: EdgeInsets.only(left: 20.1, top: 20.1, bottom: 20.1),
-              child: EmotionTagBox(emotions: widget.emotions,),
+              child: EmotionTagBox(
+                emotions: widget.emotions,
+              ),
             ),
           ],
         ),
