acc84fb744c04edcc8e3212ff82cfa79befedd9c
강석윤 <james.kang@gooroomee.com>
2020-08-02 17:22:36 +0900

    카드 리스트 UI 완료!!

--- lib/components/record_card.dart
+++ lib/components/record_card.dart
@@ -15,7 +15,7 @@ class RecordCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
         shape:
-        RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
         color: Color(0xff2a67d0),
         child: Padding(
           padding: EdgeInsets.all(20),
@@ -38,74 +38,23 @@ class RecordCard extends StatelessWidget {
                         padding: EdgeInsets.only(left: 20, right: 10),
                         child: Column(
                           children: <Widget>[
-                            Container( // TODO: ...
-                              height: this.record.emotions.isNotEmpty ? 130 : 0,
-                              child: GridView.count(
-                                crossAxisCount: 3,
-                                shrinkWrap: true,
-                                crossAxisSpacing: 25,
-                                mainAxisSpacing: 12,
-                                childAspectRatio: MediaQuery
-                                    .of(context)
-                                    .size
-                                    .width /
-                                    (MediaQuery
-                                        .of(context)
-                                        .size
-                                        .height / 3),
-                                children: List.generate(
-                                    this.record.emotions.length, (index) {
-                                  return _textInsideCircleWidget(
-                                      this.record.emotions[index].name);
-                                }),
-                              ),
-                            ),
-                            Padding(
-                                padding: EdgeInsets.only(
-                                    top: this.record.emotions.isNotEmpty
-                                        ? 13
-                                        : 0),
-                                child:
-                                Container( // TODO: ...
-                                  height: this.record.tags.isNotEmpty
-                                      ? 130
-                                      : 0,
-                                  child: GridView.count(
-                                    crossAxisCount: 3,
-                                    shrinkWrap: true,
-                                    crossAxisSpacing: 25,
-                                    mainAxisSpacing: 12,
-                                    childAspectRatio: MediaQuery
-                                        .of(context)
-                                        .size
-                                        .width /
-                                        (MediaQuery
-                                            .of(context)
-                                            .size
-                                            .height / 3),
-                                    children: List.generate(
-                                        this.record.tags.length, (index) {
-                                      return _textWithHashTagWidget(
-                                          this.record.tags[index].name);
-                                    }),
-                                  ),
-                                ),
-                            ),
+                            emotionWidget(),
+                            tagWidget(),
                             Expanded(
                                 child: Padding(
-                                  padding: EdgeInsets.only(
-                                      top: this.record.emotions.isNotEmpty ||
+                              padding: EdgeInsets.only(
+                                  top: this.record.emotions.isNotEmpty ||
                                           this.record.tags.isNotEmpty
-                                          ? 13
-                                          : 0),
-                                  child: Text(
-                                    record.description,
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 14,
-                                        fontFamily: "NotoSansKR"),
-                                  ),
-                                )),
+                                      ? 13
+                                      : 0),
+                              child: Text(
+                                record.description,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    fontFamily: "NotoSansKR"),
+                              ),
+                            )),
                             Padding(
                                 padding: EdgeInsets.only(top: 25, right: 10),
                                 child: Align(
@@ -127,36 +76,63 @@ class RecordCard extends StatelessWidget {
           ),
         ));
   }
-}
 
-Widget _textInsideCircleWidget(String text) {
-  return ClipRect(
-      child: Container(
-          height: 27,
-          width: text.length * 13.0 + 30,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.white, width: 1.5),
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15.0),
-                topRight: Radius.circular(15.0),
-                bottomLeft: Radius.circular(15.0),
-                bottomRight: Radius.circular(15.0)),
-          ),
-          // this line makes the coffee.
-          child: Center(
-              child: Text(
-                text,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: "NotoSans",
-                    fontWeight: FontWeight.w700),
-              ))));
-}
+  tagWidget() {
+    if (this.record.tags.isEmpty) {
+      return Container();
+    }
+    return Padding(
+      padding: EdgeInsets.only(top: this.record.emotions.isNotEmpty ? 18 : 0, right: 40),
+      child: Wrap(
+        spacing: 8.0,
+        runSpacing: 4.0,
+        children: List.generate(this.record.tags.length, (index) {
+          return Text("#" + this.record.tags[index].name + " ",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontFamily: "NotoSans",
+                  fontWeight: FontWeight.w700));
+        }),
+      ),
+    );
+  }
 
-_textWithHashTagWidget(String text) {
-  return Text("#" + text,
-      style: TextStyle(
-          color: Colors.white,
-          fontFamily: "NotoSans",
-          fontWeight: FontWeight.w700));
+  emotionWidget() {
+    if (this.record.emotions.isEmpty) {
+      return Container();
+    }
+
+    return Wrap(
+      spacing: 8.0,
+      runSpacing: 8.0,
+      children: List.generate(this.record.emotions.length, (index) {
+        return _textInsideCircleWidget(this.record.emotions[index].name);
+      }),
+    );
+  }
+
+  Widget _textInsideCircleWidget(String text) {
+    return ClipRect(
+        child: Container(
+            height: 27,
+            width: text.length * 14.0 + 30,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white, width: 1.5),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0)),
+            ),
+            // this line makes the coffee.
+            child: Center(
+                child: Text(
+              text,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "NotoSans",
+                  fontWeight: FontWeight.w700),
+            ))));
+  }
 }
