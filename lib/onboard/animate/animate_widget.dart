ff7aed77d9975db1c572bc20e12f019c8bfb7f33
kys6879 <kys6879@naver.com>
2020-08-06 21:29:53 +0900

    #74 enable debug function

--- lib/onboard/animate/animate_widget.dart
+++ lib/onboard/animate/animate_widget.dart
@@ -38,10 +38,12 @@ class OnboardAnimateState extends State<OnboardAnimate>
   Animation<double> startBtnAnimation;
   Animation<double> circleAnimation;
 
-  int animateStep = 1;
+  int animateStep = 5;
   double contentMargin = 0;
 
   TimePickerProvider _timePickerProvider;
+
+  bool isFocusStep = false;
 
   void initState() {
     super.initState();
@@ -189,14 +191,13 @@ class OnboardAnimateState extends State<OnboardAnimate>
   }
 
   Widget buildStep1() {
-
     final width = MediaQuery.of(context).size.width;
 
     title_widget = Text(
       "Hello",
       textAlign: TextAlign.center,
       style: TextStyle(
-        fontSize: width*0.2,
+        fontSize: width * 0.2,
         color: Colors.white,
         fontWeight: FontWeight.w100,
       ),
@@ -205,7 +206,7 @@ class OnboardAnimateState extends State<OnboardAnimate>
     message_widget = Text(
       "안녕하세요",
       style: TextStyle(
-        fontSize: width*0.07,
+        fontSize: width * 0.07,
         color: hexToColor("#e4faff"),
         fontWeight: FontWeight.bold,
       ),
@@ -218,18 +219,22 @@ class OnboardAnimateState extends State<OnboardAnimate>
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(top: 77),
-            child: FadeTransition(
-              opacity: titleAnimation,
-              child: title_widget,
-            ),
+            child: this.isFocusStep
+                ? title_widget
+                : FadeTransition(
+                    opacity: titleAnimation,
+                    child: title_widget,
+                  ),
           ),
           SizedBox(
             height: 77,
           ),
-          FadeTransition(
-            opacity: messageAnimation,
-            child: message_widget,
-          )
+          this.isFocusStep
+              ? message_widget
+              : FadeTransition(
+                  opacity: messageAnimation,
+                  child: message_widget,
+                )
         ],
       ),
     );
@@ -260,18 +265,22 @@ class OnboardAnimateState extends State<OnboardAnimate>
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(top: 30),
-            child: FadeTransition(
-              opacity: titleAnimation,
-              child: title_widget,
-            ),
+            child: this.isFocusStep
+                ? title_widget
+                : FadeTransition(
+                    opacity: titleAnimation,
+                    child: title_widget,
+                  ),
           ),
           SizedBox(
             height: 30.0,
           ),
-          FadeTransition(
-            opacity: messageAnimation,
-            child: message_widget,
-          )
+          this.isFocusStep
+              ? message_widget
+              : FadeTransition(
+                  opacity: messageAnimation,
+                  child: message_widget,
+                )
         ],
       ),
     );
@@ -301,18 +310,22 @@ class OnboardAnimateState extends State<OnboardAnimate>
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(top: 186),
-            child: FadeTransition(
-              opacity: titleAnimation,
-              child: title_widget,
-            ),
+            child: this.isFocusStep
+                ? title_widget
+                : FadeTransition(
+                    opacity: titleAnimation,
+                    child: title_widget,
+                  ),
           ),
           SizedBox(
             height: 20.0,
           ),
-          FadeTransition(
-            opacity: messageAnimation,
-            child: message_widget,
-          )
+          this.isFocusStep
+              ? message_widget
+              : FadeTransition(
+                  opacity: messageAnimation,
+                  child: message_widget,
+                )
         ],
       ),
     );
@@ -321,6 +334,51 @@ class OnboardAnimateState extends State<OnboardAnimate>
   Widget buildStep4() {
     contentMargin = 20.0;
     title_widget = Image.asset('lib/src/image/onboarding/contents_img_03.png');
+
+    Widget step4_text_endAt = Container(
+      padding: EdgeInsets.only(left: 60, right: 60),
+      child: Text(
+        "그 전에 한 가지 알려주세요.\n당신의 하루가 끝나는 시간을\n언제로 설정하면 좋을까요?",
+        style: TextStyle(
+          fontSize: 20,
+          color: hexToColor("#e4faff"),
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+
+    Widget step4_btn_picker =
+        ButtonTheme(minWidth: 200, height: 56, child: TimePickerWidget());
+
+    Widget step4_btn_next = ButtonTheme(
+      minWidth: 280,
+      height: 50,
+      child: FlatButton(
+        color: Colors.transparent,
+        onPressed: () {
+          reverseStep4();
+        },
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              "다음으로",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: hexToColor("#92d8ff")),
+            ),
+            Icon(Icons.forward, color: hexToColor("#92d8ff")),
+          ],
+        ),
+      ),
+    );
+
+    Widget step4_text_after_setting = Text(
+      "* 추후에 설정탭에서 변경 가능합니다.",
+      style: TextStyle(fontSize: 13, color: Colors.white.withOpacity(0.5)),
+    );
 
     return Center(
       child: Column(
@@ -331,68 +389,39 @@ class OnboardAnimateState extends State<OnboardAnimate>
           SizedBox(
             height: 165.0,
           ),
-          FadeTransition(
-            opacity: step4MessageAnimation,
-            child: Container(
-              padding: EdgeInsets.only(left: 60, right: 60),
-              child: Text(
-                "그 전에 한 가지 알려주세요.\n당신의 하루가 끝나는 시간을\n언제로 설정하면 좋을까요?",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: hexToColor("#e4faff"),
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
+          this.isFocusStep
+              ? step4_text_endAt
+              : FadeTransition(
+                  opacity: step4MessageAnimation, child: step4_text_endAt),
           SizedBox(
             height: 50.0,
           ),
-          FadeTransition(
-            opacity: pickerAnimation,
-            child: ButtonTheme(
-                minWidth: 200, height: 56, child: TimePickerWidget()),
-          ),
+          this.isFocusStep
+              ? step4_btn_picker
+              : FadeTransition(
+                  opacity: pickerAnimation,
+                  child: ButtonTheme(
+                      minWidth: 200, height: 56, child: TimePickerWidget()),
+                ),
           SizedBox(
             height: 90.0,
           ),
-          FadeTransition(
-            opacity: nextBtnAnimation,
-            child: ButtonTheme(
-              minWidth: 280,
-              height: 50,
-              child: FlatButton(
-                color: Colors.transparent,
-                onPressed: () {
-                  reverseStep4();
-                },
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      "다음으로",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: hexToColor("#92d8ff")),
-                    ),
-                    Icon(Icons.forward, color: hexToColor("#92d8ff")),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          this.isFocusStep
+              ? step4_btn_next
+              : FadeTransition(
+                  opacity: nextBtnAnimation, child: step4_btn_next),
           SizedBox(
             height: 35.0,
           ),
-          FadeTransition(
-              opacity: supportMessageAnimation,
-              child: Text(
-                "* 추후에 설정탭에서 변경 가능합니다.",
-                style: TextStyle(
-                    fontSize: 13, color: Colors.white.withOpacity(0.5)),
-              ))
+          this.isFocusStep
+              ? step4_text_after_setting
+              : FadeTransition(
+                  opacity: supportMessageAnimation,
+                  child: Text(
+                    "* 추후에 설정탭에서 변경 가능합니다.",
+                    style: TextStyle(
+                        fontSize: 13, color: Colors.white.withOpacity(0.5)),
+                  ))
         ],
       ),
     );
@@ -401,17 +430,36 @@ class OnboardAnimateState extends State<OnboardAnimate>
   Widget buildStep5() {
     contentMargin = 20.0;
     print("buildStep5!");
+
+    Widget step5_text1 = Container(
+      padding: EdgeInsets.only(left: 60, right: 60),
+      child: Text(
+        "좋았어요.\n그럼 이제부터 다이브와 함께\n당신의 감정에 집중해보세요.",
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+
     return Center(
         child: Stack(
       children: <Widget>[
-        Container(
-            padding: EdgeInsets.only(top: 80),
-            child: ScaleTransition(
-              scale: circleAnimation,
-              alignment: Alignment.center,
-              child:
-                  Image.asset('lib/src/image/onboarding/contents_img_04.png'),
-            )),
+        this.isFocusStep
+            ? Container(
+                padding: EdgeInsets.only(top: 80),
+                child:
+                    Image.asset('lib/src/image/onboarding/contents_img_04.png'))
+            : Container(
+                padding: EdgeInsets.only(top: 80),
+                child: ScaleTransition(
+                  scale: circleAnimation,
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                      'lib/src/image/onboarding/contents_img_04.png'),
+                )),
         Column(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.start,
@@ -420,25 +468,17 @@ class OnboardAnimateState extends State<OnboardAnimate>
             SizedBox(
               height: 219.0,
             ),
-            FadeTransition(
-              opacity: step5MessageAnimation,
-              child: Container(
-                padding: EdgeInsets.only(left: 60, right: 60),
-                child: Text(
-                  "좋았어요.\n그럼 이제부터 다이브와 함께\n당신의 감정에 집중해보세요.",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
+            this.isFocusStep
+                ? step5_text1
+                : FadeTransition(
+                    opacity: step5MessageAnimation, child: step5_text1),
             SizedBox(
               height: 160.0,
             ),
-            FadeTransition(opacity: startBtnAnimation, child: StartDiveWidget())
+            this.isFocusStep
+                ? StartDiveWidget()
+                : FadeTransition(
+                    opacity: startBtnAnimation, child: StartDiveWidget())
           ],
         ),
       ],
@@ -476,15 +516,24 @@ class OnboardAnimateState extends State<OnboardAnimate>
     }
   }
 
+  void debug_enable_focusStep() {
+    this.isFocusStep = true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    /* step을 setState. -> 애니메이션 실행 */
-    if (animateStep < 4) {
-      Future.delayed(const Duration(milliseconds: 1000), () {
-        messageController.forward();
-      });
-    }
     this._timePickerProvider = Provider.of<TimePickerProvider>(context);
+
+    debug_enable_focusStep();
+
+    if (this.isFocusStep == false) {
+      /* step을 setState. -> 애니메이션 실행 */
+      if (animateStep < 4) {
+        Future.delayed(const Duration(milliseconds: 1000), () {
+          messageController.forward();
+        });
+      }
+    }
 
     return buildAnimate(animateStep);
   }
