b22950b0b7c3070dd60457ccf03c520cd06e535a
kys6879 <kys6879@naver.com>
2020-08-25 00:35:27 +0900

    #147 remove space

--- lib/components/input/step3/edit_tag_list.dart
+++ lib/components/input/step3/edit_tag_list.dart
@@ -73,9 +73,10 @@ class _EditTagListState extends State<EditTagList> {
                                     "[edit_tag_list.dart] _showAddTagDialog then -> ${value.value}");
 
                                 if (value.isConfirm) {
+                                  final nameParam = value.value.split(" ").join("");
                                   Tag tagParam = Tag(
                                     id: CommonService.generateUUID(),
-                                    name: value.value
+                                    name: nameParam
                                   );
                                   final exist = this._tags.firstWhere((tag) => tag.name == tagParam.name, orElse: () => null);
                                   if(exist == null) {
