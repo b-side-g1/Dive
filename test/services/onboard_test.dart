4393629e2268d745471a0b3ed06332b267ea0550
Terry.Joo <jooyungik@gmail.com>
2020-09-06 02:34:36 +0900

    Move tests

--- test/services/onboard_test.dart
+++ test/services/onboard_test.dart
@@ -1,20 +1,16 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'package:test/test.dart';
 import 'package:flutterapp/models/basic_model.dart';
-import 'package:flutterapp/services/basic/basic_service.dart';
-import 'package:sqflite_common/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:matcher/src/equals_matcher.dart';
-import 'package:path_provider/path_provider.dart';
+
+import '../config_test.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   test('BASIC 테이블 쿼리', () async {
     sqfliteFfiInit();
-    String path = "D:\\android\\diary-app\\diary_app_database.db";
+    String path = TEST_DATABASE_PATH;
 
     var db = await databaseFactoryFfi.openDatabase(path);
 
--- test/services/tag_test.dart
+++ test/services/tag_test.dart
@@ -1,15 +1,7 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/models/tag_model.dart';
-import 'package:flutterapp/services/tag/tag_service.dart';
-import 'package:path/path.dart';
-import 'package:test/test.dart';
-import 'package:flutterapp/models/basic_model.dart';
-import 'package:flutterapp/services/basic/basic_service.dart';
-import 'package:sqflite_common/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:matcher/src/equals_matcher.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:test/test.dart';
 
 Future<void> main() async {
 
