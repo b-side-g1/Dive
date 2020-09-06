9dcf17c2aef5fa821e0ab8305ef1907d7408e605
youngseoKim <kys6879@naver.com>
2020-09-06 23:42:39 +0900

    #179 [입력페이지] 이유태그 UI 잘려보임 개선 (#180)
    * #179 relative step3
    
    * #179 layout
    
    * #179 add scroll bar

--- lib/components/input/step3/reason_tag_widget.dart
+++ lib/components/input/step3/reason_tag_widget.dart
@@ -33,70 +33,76 @@ class _BuildReasonTagState extends State<ReasonTagWidget> {
   Widget build(BuildContext context) {
     print("[reason_tag_widget.dart] #build! ");
 
+    final _scrollController = ScrollController();
+
     final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
 
     final container = StateContainer.of(context);
     setState(() {
       this.selectedCount = container.tags.length;
       this.selectedTags = container.tags;
 
-      if(widget.tags != null){
+      if (widget.tags != null) {
         widget.tags.asMap().forEach((index, tag) => {
-            this.selectedTags.forEach((element) {
-                if(tag.id == element.id) {
+              this.selectedTags.forEach((element) {
+                if (tag.id == element.id) {
                   this.selecteds[index] = true;
                 }
-            })
-        });
+              })
+            });
       }
     });
 
     return widget.tags == null
         ? Text("")
-        : SingleChildScrollView(
-          child: Wrap(
-              spacing: 8.0,
-              children: List.generate(widget.tags.length, (index) {
-                return ButtonTheme(
-                    minWidth: width * 0.19,
-                    height: 30,
-                    child: FlatButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100.0),
-                      ),
-                      color: selecteds[index]
-                          ? CommonService.hexToColor("#63c7ff")
-                          : CommonService.hexToColor("#2f4262"),
-                      textColor: Colors.white,
-                      onPressed: () {
-                        setState(() {
-                          if (selecteds[index]) {
-                            selectedTags.remove(widget.tags[index]);
-                            selectedCount--;
-                            selecteds[index] = false;
-                            selectedTags.removeWhere((tag) => tag.id == widget.tags[index].id);
-
-                          } else {
-                            if (selectedCount >= 5) {
-                              return CommonService.showToast("5개까지만 선택이 가능합니다.");
-                            }
-                            selectedTags.add(widget.tags[index]);
-                            selectedCount++;
-                            selecteds[index] = true;
-
-                          }
-                          container.updateTags(selectedTags);
-                        });
-                      },
-                      child: Text(
-                        "${widget.tags[index].name}",
-                        style: TextStyle(
-                          fontSize: width * 0.04,
-                        ),
-                      ),
-                    ));
-              })),
-        );
+        : Scrollbar(
+            controller: _scrollController,
+            isAlwaysShown: true,
+            child: SingleChildScrollView(
+              controller: _scrollController,
+              child: Wrap(
+                  spacing: 5.0,
+                  children: List.generate(widget.tags.length, (index) {
+                    return ButtonTheme(
+                        minWidth: width * 0.19,
+                        height: 30,
+                        child: FlatButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100.0),
+                          ),
+                          color: selecteds[index]
+                              ? CommonService.hexToColor("#63c7ff")
+                              : CommonService.hexToColor("#2f4262"),
+                          textColor: Colors.white,
+                          onPressed: () {
+                            setState(() {
+                              if (selecteds[index]) {
+                                selectedTags.remove(widget.tags[index]);
+                                selectedCount--;
+                                selecteds[index] = false;
+                                selectedTags.removeWhere(
+                                    (tag) => tag.id == widget.tags[index].id);
+                              } else {
+                                if (selectedCount >= 5) {
+                                  return CommonService.showToast(
+                                      "5개까지만 선택이 가능합니다.");
+                                }
+                                selectedTags.add(widget.tags[index]);
+                                selectedCount++;
+                                selecteds[index] = true;
+                              }
+                              container.updateTags(selectedTags);
+                            });
+                          },
+                          child: Text(
+                            "${widget.tags[index].name}",
+                            style: TextStyle(
+                              fontSize: width * 0.04,
+                            ),
+                          ),
+                        ));
+                  })),
+            ),
+          );
   }
 }
