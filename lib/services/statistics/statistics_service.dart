16f3f1384a544d1f9eb09d8e554da2748e1f63fe
Terry <yungik@glocalizeinc.com>
2020-10-01 17:16:04 +0900

    Implement query for graph
    week(x축): 7, 14, 21, 28~31
    score(y축): 주별로 묶인 평균 점수
    
    기타 설명
    쿼리 실행 시 행의 수
    - 최소: 0
    - 최대: 4

--- lib/services/statistics/statistics_service.dart
+++ lib/services/statistics/statistics_service.dart
@@ -1,3 +1,4 @@
+import 'package:Dive/commons/function.dart';
 import 'package:Dive/models/daily_model.dart';
 import 'package:Dive/models/record_model.dart';
 import 'package:Dive/services/database/database_helper.dart';
@@ -19,34 +20,40 @@ class StatisticsService {
     final db = await DBHelper().database;
     return db
         .query(Daily.tableName,
-        columns: ['id'], where: 'month = $month AND year = $year')
-        .then((ids) async =>
-        db
-            .query(Record.tableName,
-            where:
-            "dailyId IN('${ids.map((e) => e['id']).join("', '")}')")
-            .then((records) {
-          var sum = records
-              .map((e) =>
-          Record
-              .fromJson(e)
-              .score)
-              .reduce((a, b) => a + b);
-          print(sum);
-          return sum / records.length;
-        }));
+            columns: ['id'], where: 'month = $month AND year = $year')
+        .then((ids) async => db
+                .query(Record.tableName,
+                    where:
+                        "dailyId IN('${ids.map((e) => e['id']).join("', '")}')")
+                .then((records) {
+              var sum = records
+                  .map((e) => Record.fromJson(e).score)
+                  .reduce((a, b) => a + b);
+              print(sum);
+              return sum / records.length;
+            }));
   }
 
 //  Future<List<GraphDto>> getGraphData([int month, int year]) async {
-  getGraphData([int month, int year]) async {
+  Future<List<Map<String, dynamic>>> getGraphData([int month, int year]) async {
     month = month ?? DateTime.now().month;
     year = year ?? DateTime.now().year;
     final db = await DBHelper().database;
-    var result = await db.rawQuery("""
-    SELECT day FROM Daily WHERE month = ? AND year = ?
-    GROUP BY year, month, day
-    """, [month, year]);
-    throw result;
-    return result;
+    return db.rawQuery("""
+    SELECT week, CAST(SUM(sum) AS REAL) / SUM(cnt) score
+    FROM (SELECT CASE WHEN 7 >= day THEN 7
+                      WHEN day BETWEEN 8 AND 14 THEN 14
+                      WHEN day BETWEEN 15 AND 21 THEN 21
+                      ELSE ?
+                 END week,
+                 SUM(r.score) sum,
+                 COUNT(r.id) cnt
+         FROM daily d JOIN record r ON d.id = r.dailyId
+         WHERE d.month = ? AND d.year = ?
+         GROUP BY d.year, d.month, d.day) t
+    GROUP BY week
+    ORDER BY week
+    """,
+        [getLastDay(month, year), month, year]).then((value) => value.toList());
   }
 }
